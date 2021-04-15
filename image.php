963a98d8e8e5652e720f5dea87b03429206780f3
ecwpz91 <ecwpz91@users.noreply.github.com>
2021-04-15 07:24:52 -0400

    Added ternary operator for server variable

--- image.php
+++ image.php
@@ -4,15 +4,16 @@ $im = imagecreatetruecolor(200, 200);
 $white = imagecolorallocate($im, 255, 255, 255);
 $blue = imagecolorallocate($im, 0, 0, 255);
 $green = imagecolorallocate($im, 0, 255, 0);
-$color = getenv('COLOR', true) ?: getenv('COLOR');
+$color = isset($_SERVER['COLOR'])?$_SERVER['COLOR']:"BLUE";
 
-if($color == 'WHITE') {
-    imagefilledrectangle($im, 0, 0, 199, 199, $white);
+if($color == 'BLUE') {
+    imagefilledrectangle($im, 0, 0, 199, 199, $blue);
 } else if($color  == 'GREEN') {
     imagefilledrectangle($im, 0, 0, 199, 199, $green);
 } else {
-    imagefilledrectangle($im, 0, 0, 199, 199, $blue);
+    imagefilledrectangle($im, 0, 0, 199, 199, $white);
 }
+
 // Save the image
 header('Content-Type: image/png');
 imagePNG($im);

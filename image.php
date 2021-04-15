823100890b43d01c5492eff9f6303a153234c9ca
ecwpz91 <ecwpz91@users.noreply.github.com>
2021-04-15 07:09:06 -0400

    Added if, then, else conditional statement

--- image.php
+++ image.php
@@ -4,11 +4,15 @@ $im = imagecreatetruecolor(200, 200);
 $white = imagecolorallocate($im, 255, 255, 255);
 $blue = imagecolorallocate($im, 0, 0, 255);
 $green = imagecolorallocate($im, 0, 255, 0);
+$color = getenv('COLOR', true) ?: getenv('COLOR');
 
-// // Draw a filled rectangle
-imagefilledrectangle($im, 0, 0, 199, 199, $blue);
-// imagefilledrectangle($im, 0, 0, 199, 199, $green);
-
+if($color == 'WHITE') {
+    imagefilledrectangle($im, 0, 0, 199, 199, $white);
+} else if($color  == 'GREEN') {
+    imagefilledrectangle($im, 0, 0, 199, 199, $green);
+} else {
+    imagefilledrectangle($im, 0, 0, 199, 199, $blue);
+}
 // Save the image
 header('Content-Type: image/png');
 imagePNG($im);
